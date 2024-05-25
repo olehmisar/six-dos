70ffaf151592f758b4da213923e13f48ae99aeac
Oleh Misarosh <olehmisar@gmail.com>
2024-05-26 00:35:39 +0200

    test: failing test

--- packages/contracts/src/links.test.ts
+++ packages/contracts/src/links.test.ts
@@ -1,5 +1,11 @@
+import { getSchnorrAccount } from "@aztec/accounts/schnorr";
 import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
-import { type AccountWallet, type PXE } from "@aztec/aztec.js";
+import {
+  Fr,
+  GrumpkinScalar,
+  type AccountWallet,
+  type PXE,
+} from "@aztec/aztec.js";
 import { beforeAll, describe, expect, test } from "vitest";
 import { deployContracts } from "./deployContracts.js";
 import { EventContract } from "./event/target/Event.js";
@@ -8,12 +14,16 @@ import { SixDosSdk, getPxe } from "./sdk.js";
 
 describe("Bridge", () => {
   let pxe: PXE;
-  let eventOwner: AccountWallet, alice: AccountWallet, bob: AccountWallet;
+  let eventOwner: AccountWallet,
+    alice: AccountWallet,
+    bob: AccountWallet,
+    charlie: AccountWallet;
   let contracts: { links: LinksContract; event: EventContract };
   let sdk: SixDosSdk;
   beforeAll(async () => {
     pxe = await getPxe();
     [eventOwner, alice, bob] = await getInitialTestAccountsWallets(pxe);
+    charlie = await createAccount(pxe);
     const deployer = eventOwner;
     contracts = await deployContracts(deployer);
     sdk = new SixDosSdk(async () => contracts);
@@ -26,6 +36,7 @@ describe("Bridge", () => {
     );
     await sdk.addLink(eventOwner, alice.getAddress());
     await sdk.addLink(alice, bob.getAddress());
+    await sdk.addLink(bob, charlie.getAddress());
 
     await sdk.invite(eventOwner, alice.getAddress());
     await sdk.invite(alice, bob.getAddress());
@@ -39,4 +50,21 @@ describe("Bridge", () => {
     expect(aliceDegree).toBe(1n);
     expect(bobDegree).toBe(2n);
   });
+
+  test("fails if degree is too high", async () => {
+    await expect(sdk.invite(bob, charlie.getAddress())).rejects.toThrow(
+      "Cannot satisfy constraint",
+    );
+  });
 });
+
+async function createAccount(pxe: PXE) {
+  const secretKey = Fr.random();
+  const signingPrivateKey = GrumpkinScalar.random();
+  const account = await getSchnorrAccount(
+    pxe,
+    secretKey,
+    signingPrivateKey,
+  ).waitSetup();
+  return account;
+}
